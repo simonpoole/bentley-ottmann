2f7d157393fcc7cecd8d88dbc4e51ab45371286c
simonpoole <simon@poole.ch>
2026-04-02 14:04:04 +0200

    Fix Sweepline ordering

--- src/main/java/bentleyottmann/SweepLine.java
+++ src/main/java/bentleyottmann/SweepLine.java
@@ -8,8 +8,19 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 class SweepLine extends TreeSet<SweepSegment> {
+
+    private static final long serialVersionUID = 1L;
+
     SweepLine() {
-        super((SweepSegment s1, SweepSegment s2) -> Double.compare(s1.position(), s2.position()));
+        super((SweepSegment s1, SweepSegment s2) -> {
+            int posCompare = Double.compare(s1.position(), s2.position());
+            if (posCompare != 0) {
+                return posCompare;
+            }
+            // If positions are equal, use object identity as tiebreaker
+            // This ensures segments at the same y-coordinate are still kept separate
+            return Integer.compare(System.identityHashCode(s1), System.identityHashCode(s2));
+        });
     }
 
     void remove(@NotNull SweepSegment s) {
@@ -24,7 +35,7 @@ class SweepLine extends TreeSet<SweepSegment> {
     void swap(@NotNull SweepSegment s1, @NotNull SweepSegment s2) {
         remove(s1);
         remove(s2);
-System.out.println("SweepLine swap");
+        System.out.println("SweepLine swap");
         final double swap = s1.position();
         s1.setPosition(s2.position());
         s2.setPosition(swap);
